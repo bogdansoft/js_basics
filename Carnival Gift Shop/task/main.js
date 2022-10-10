050aa627ecec059acc24fa933f71465df996771b
bsyso <bsyso@softserveinc.com>
2022-10-10 19:19:31 +0200

    added 2 task

--- Carnival Gift Shop/task/main.js
+++ Carnival Gift Shop/task/main.js
@@ -1,13 +1,49 @@
 console.log("WELCOME TO THE CARNIVAL GIFT SHOP!");
 console.log("Hello friend! Thank you for visiting the carnival!");
 console.log("Here's the list of gifts:\n");
-console.log("Teddy Bear");
-console.log("Big Red Ball");
-console.log("Huge Bear");
-console.log("Candy");
-console.log("Stuffed Tiger");
-console.log("Stuffed Dragon");
-console.log("Skateboard");
-console.log("Toy Car");
-console.log("Basketball");
-console.log("Scary Mask");
+
+let array = [
+    {
+        name: "Teddy Bear",
+        price: 10
+    },
+    {
+        name: "Big Red Ball",
+        price: 5
+    },
+    {
+        name: "Huge Bear",
+        price: 50
+    },
+    {
+        name: "Candy",
+        price: 8
+    },
+    {
+        name: "Stuffed Tiger",
+        price: 15
+    },
+    {
+        name: "Stuffed Dragon",
+        price: 30
+    },
+    {
+        name: "Skateboard",
+        price: 100
+    },
+    {
+        name: "Toy Car",
+        price: 25
+    },
+    {
+        name: "Basketball",
+        price: 20
+    },
+    {
+        name: "Scary Mask",
+        price: 75
+    }
+];
+for (let i = 0; i < array.length; i++) {
+    console.log(`${i + 1}- ${array[i].name}, Cost: ${array[i].price} tickets`);
+}
